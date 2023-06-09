5b02767542079cdbf8dd196b2775b37e95dbc417
KURDYK <lkurdyk@c2s.fr>
2023-06-09 15:57:18 +0200

    switchable component basic css

--- src/components/roads/index.tsx
+++ src/components/roads/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import SwitchableComponent from 'utils/switchableComponent'
 import { useData } from './hook'
 import SearchableDataGridComponent from 'utils/SearchableDataGrid'
+import { Box } from '@mui/material'
 
 const RoadComponent: React.FC = () => {
 
@@ -11,11 +12,15 @@ const RoadComponent: React.FC = () => {
     console.log(displayableRoads)
 
     return (
-        <SwitchableComponent 
-            components={[{element:<SearchableDataGridComponent rows={displayableRoads} columns={columns} />, label:"button"},
-                {element: <Greeter name={'toto'} />, label:"greeter"}]} 
-            defaultComponent={0} 
-        />)
+        <Box id="SwitchableRoadDisplay">
+            <SwitchableComponent
+                components={[
+                    {element:<SearchableDataGridComponent rows={displayableRoads} columns={columns} />, label:"button"},
+                    {element: <Greeter name={'toto'} />, label:"greeter"}]} 
+                defaultComponent={0} 
+            />
+        </Box>
+    )
 }
 
 export default RoadComponent;
